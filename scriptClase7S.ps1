c0cf23e3a84e6cbb42b49acd4da1189f075aeeeb
NachoMol <ignaciotmolina@gmail.com>
2023-03-01 20:12:57 -0300

    subiendo caso nombre ni con L ni con A

--- scriptClase7S.ps1
+++ scriptClase7S.ps1
@@ -1,6 +1,7 @@
 $req = Invoke-WebRequest -Method Get -Uri https://raw.githubusercontent.com/olea/lemarios/master/nombres-propios-es.txt
 $nombresConA = $req.Content.split("`n") | Where-Object { $_ -like "A*" }
 $nombresConL = $req.Content.split("`n") | Where-Object { $_ -like "L*" }
+$nombresDistintosAL = $req.Content.split("`n") | Where-Object { $_ -notlike "A*" -and $_ -notLike "L*" }
 
 1..5 | ForEach-Object {
   $random = Get-Random -Minimum 0 -Maximum $($nombresConA.count-1)
@@ -18,3 +19,10 @@ $nombresConL = $req.Content.split("`n") | Where-Object { $_ -like "L*" }
   write-output "El género probable de $nombre es $gender"
 }
 
+1..5 | ForEach-Object {
+  $random = Get-Random -Minimum 0 -Maximum $($nombresDistintosAL.count-1)
+  $nombre = $nombresDistintosAL[$random]
+  $gender = Invoke-RestMethod -Method Get -Uri https://api.genderize.io/?name=$nombre | Select-Object -ExpandProperty Gender
+  $country = Invoke-RestMethod -Method Get -Uri https://api.nationalize.io/?name=$nombre | Select-Object -ExpandProperty Country | Select-Object -First 1 -Property country_id
+  write-output "El género probable de $nombre es $gender"
+}
